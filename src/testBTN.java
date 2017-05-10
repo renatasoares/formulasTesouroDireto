30acabc7ac756d21a43bac01343d98d772d87a59
Renata Soares dos Santos <renata.sds09@gmail.com>
2017-05-10 17:37:48 -0300

    Começando os testes da classe BTN.java que representa o Bônus do Tesouro Nacional
    Testes:
     * testeAtualizacaoPU: testa o calculo da atualização do preço unitário
     * testeCalculaFatorJuros: testa o calculo do fator de juros
     * testeCalculaJuros: testa o calculo do juros a partir do fator de juros e o preço unitário atualizado
     * testeCalculaPrincipal: testa o calculo do principal a partir do preço unitário atualizado e uma quantidade.
    
    Signed-off-by: Renata Soares dos Santos <renata.sds09@gmail.com>
    Signed-off-by: Sabryna Sousa <sabryna.sousa1323@gmail.com>

---
+++ src/testBTN.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class testBTN {
+
+	BTN btn;
+	
+	@Before
+	public void setUp() throws Exception {
+		/*TR = 	0.0031;
+		 PU = 3.50
+		 juros = 9.80
+		*/
+		btn = new BTN(0.0031, 3.50, 9.80);
+	}
+	
+	@Test
+	public void testeAtualizacaoPU() {
+		double PUanterior = btn.getPU();
+		double PUatualizado = btn.atualizarPU();
+		
+		assertEquals(PUanterior * btn.getTR(), PUatualizado);
+	}
+	
+	@Test
+	public void testeCalculaFatorJuros(){
+		int numeroDeMeses = 2;
+		double fatorJurosEsperado = 1.01570375;
+		
+		assertEquals(btn.calculaFJ(numeroDeMeses), fatorJurosEsperado) ;
+	}
+	
+	@Test
+	public void testeCalculaJuros(){
+		int numeroDeMeses;
+		double fatorJuros = 1.01570375;
+		double jurosEsperado = fatorJuros * btn.getPU();
+		
+		assertEquals(btn.calculaJuros(numeroDeMeses), jurosEsperado);
+	}
+	
+	@Test
+	public void testeCalculaPrincipal(){
+		int quantidade = 3;
+		double principalEsperado = quantidade * btn.getPU();
+		
+		assertEquals(btn.calculaPrincipal(quantidade), principalEsperado);
+	}
+
+}
