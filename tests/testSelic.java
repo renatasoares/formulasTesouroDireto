277961acb985d8b85c3df2ee4e262b5d971928c9
Renata Soares dos Santos <renata.sds09@gmail.com>
2017-05-18 10:10:52 -0300

    Initializing tests of Selic class that will return the productive fator

---
+++ tests/testSelic.java
@@ -0,0 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import junit.framework.Assert;
+
+public class testSelic {
+//1.00019679
+	Selic selic;
+	
+	@Before
+	public void setUp() throws Exception {
+		selic = new Selic();
+		
+		selic.registerTaxasSelic(1.20, "02/10/10");
+		selic.registerTaxasSelic(1.50, "03/10/10");
+		selic.registerTaxasSelic(1.70, "04/10/10");
+		selic.registerTaxasSelic(1.80, "05/10/10");
+	}
+	
+	@Test
+	public void testcalculoFatorProdutorio(){
+		double fatorProdutorioEsperado = 1.00019679;
+		
+		Assert.assertEquals(fatorProdutorioEsperado, selic.calcularFatorProdutorio("03/10/10", "05/10/10"));
+	}
+
+}
